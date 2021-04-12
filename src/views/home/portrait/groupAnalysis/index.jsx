7a26eb5bec6020c64ae509ca8a06687c6dc4a173
lwrench <1243115181@qq.com>
2021-04-12 14:41:04 +0800

    temp

--- src/views/home/portrait/groupAnalysis/index.jsx
+++ src/views/home/portrait/groupAnalysis/index.jsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Descriptions } from 'antd';
+import { Column } from '@antv/g2plot';
+
+const defaultSetting = {
+  label: {
+    // 可手动配置 label 数据标签位置
+    position: 'middle', // 'top', 'bottom', 'middle',
+    // 配置样式
+    style: {
+      fill: '#FFFFFF',
+      opacity: 0.6,
+    },
+  },
+  xAxis: {
+    label: {
+      autoHide: true,
+      autoRotate: false,
+    },
+  },
+};
 
 function GroupAnalysis() {
   let history = useHistory();
+  useEffect(() => {
+    const areaPlot = new Column('groupArea', {
+      ...defaultSetting,
+      data,
+      xField: 'type',
+      yField: 'sales',
+    });
+    areaPlot.render();
+    return () => {
+      areaPlot.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    const timePlot = new Column('groupTime', {
+      ...defaultSetting,
+      data,
+      xField: 'type',
+      yField: 'sales',
+    });
+    timePlot.render();
+    return () => {
+      timePlot.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    const productPlot = new Column('groupProduct', {
+      ...defaultSetting,
+      data,
+      xField: 'type',
+      yField: 'sales',
+    });
+    productPlot.render();
+    return () => {
+      productPlot.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    const platformPlot = new Column('groupPlatform', {
+      ...defaultSetting,
+      data,
+      xField: 'type',
+      yField: 'sales',
+    });
+    platformPlot.render();
+    return () => {
+      platformPlot.destroy();
+    };
+  }, []);
   return (
     <>
       <div
@@ -32,9 +102,97 @@ function GroupAnalysis() {
         >
           返回上页
         </Button>
+        <Descriptions title="用户群信息">
+          <Descriptions.Item label="用户群名称">
+            经常购买正山小种人群
+          </Descriptions.Item>
+          <Descriptions.Item label="用户群描述">
+            经常购买商品包括元正·正山小种·临江240g/盒、元正·正山小种·红茶250g
+          </Descriptions.Item>
+          <Descriptions.Item label="触达用户量">800</Descriptions.Item>
+        </Descriptions>
+        <div>
+          <div style={{ fontSize: '20px' }}>地域分布</div>
+          <div
+            id="groupArea"
+            style={{
+              margin: '40px 0',
+            }}
+          ></div>
+        </div>
+        <div>
+          <div style={{ fontSize: '20px' }}>下单时间段统计</div>
+          <div
+            id="groupTime"
+            style={{
+              margin: '40px 0',
+            }}
+          ></div>
+        </div>
+        <div>
+          <div style={{ fontSize: '20px' }}>最常购买商品</div>
+          <div
+            id="groupProduct"
+            style={{
+              margin: '40px 0',
+            }}
+          ></div>
+        </div>
+        <div>
+          <div style={{ fontSize: '20px' }}>平台统计</div>
+          <div
+            id="groupPlatform"
+            style={{
+              margin: '40px 0',
+            }}
+          ></div>
+        </div>
       </div>
     </>
   );
 }
 
 export default GroupAnalysis;
+
+const data = [
+  {
+    type: '家具家电',
+    sales: 38,
+  },
+  {
+    type: '粮油副食',
+    sales: 52,
+  },
+  {
+    type: '生鲜水果',
+    sales: 61,
+  },
+  {
+    type: '美容洗护',
+    sales: 145,
+  },
+  {
+    type: '母婴用品',
+    sales: 48,
+  },
+  {
+    type: '进口食品',
+    sales: 38,
+  },
+  {
+    type: '食品饮料',
+    sales: 38,
+  },
+  {
+    type: '家庭清洁1',
+    sales: 38,
+  },
+  {
+    type: '食品饮料1',
+    sales: 38,
+  },
+  {
+    type: '家庭清洁2',
+    sales: 38,
+  },
+];
