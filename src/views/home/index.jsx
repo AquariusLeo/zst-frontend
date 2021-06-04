962bd85ddb6c2b38186f568d90ac42770634e180
lwrench <1243115181@qq.com>
2021-06-04 19:05:17 +0800

    old

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -72,15 +72,16 @@ const Home = () => {
             <Link to={`${url}/dashboard`}>主页</Link>
           </Menu.Item>
           <SubMenu key="analysis" icon={<PieChartOutlined />} title="数据中心">
+            <Menu.Item key="analysisByProduct">
+              <Link to={`${url}/analysisByProduct`}>产品维度</Link>
+            </Menu.Item>
             <Menu.Item key="analysisByArea">
               <Link to={`${url}/analysisByArea`}>地区维度</Link>
             </Menu.Item>
             <Menu.Item key="analysisByPlatform">
               <Link to={`${url}/analysisByPlatform`}>平台维度</Link>
             </Menu.Item>
-            <Menu.Item key="analysisByProduct">
-              <Link to={`${url}/analysisByProduct`}>产品维度</Link>
-            </Menu.Item>
+
             <Menu.Item key="analysisByTime">
               <Link to={`${url}/analysisByTime`}>时间维度</Link>
             </Menu.Item>
